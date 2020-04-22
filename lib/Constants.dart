0114156dd242c4743c728c426d70897414240c3e
dammyololade <dammyololade2010@gmail.com>
2020-04-22 18:50:55 +0100

    updaed ignore

--- lib/Constants.dart
+++
@@ -1,8 +0,0 @@
-/// description:
-/// project: travel_diary
-/// @package:
-/// @author: dammyololade
-/// created on: 22/04/2020
-class Constants {
-  static const String FCM_APi = "AIzaSyB5Tl3ge9gMZ8pOcwPWlSBliJamOCtQch0";
-}
