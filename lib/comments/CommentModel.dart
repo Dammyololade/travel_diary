7b38de0d90184af166ec4a905997a3381427821f
dammyololade <dammyololade2010@gmail.com>
2020-05-24 20:29:50 +0100

    implement toMap function on comment class

--- lib/comments/CommentModel.dart
+++ lib/comments/CommentModel.dart
@@ -1,9 +1,17 @@
+import 'package:traveldiary/SessionManager.dart';
+
 /// description:
 /// project: travel_diary
 /// @package: comments
 /// @author: dammyololade
 /// created on: 07/05/2020
 class CommentModel {
+  static const String cUserName = "userName";
+  static const String cComment = "comment";
+  static const String cDate = "date";
+  static const String cUserId = "userId";
+  static const String cJournalId = "journalId";
+  static const String cRatings = "ratings";
   String userDocId, userName, comment, date, journalId;
   double ratings;
 
@@ -12,6 +20,11 @@ class CommentModel {
 
   Map<String, dynamic> toMap() {
     return {
+      cComment: comment,
+      cDate: DateTime.now(),
+      cUserId: SessionManager.documentId,
+      cJournalId: journalId,
+      cRatings: ratings
     };
   }
 }
